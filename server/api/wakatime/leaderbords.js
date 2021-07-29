bae93861dae8cc81e8dc940e7d45837872f49e22
Kananek Thongkam <info.dvgamer@gmail.com>
2021-07-30 01:01:49 +0700

    add logging.

--- server/api/wakatime/leaderbords.js
+++ server/api/wakatime/leaderbords.js
@@ -1,5 +1,7 @@
 const axios = require('axios')
 const wakaRank = require('./waka-rank')
+const logger = require('@touno-io/debuger')(`Waka`)
+
 
 const wakaApiLeaderboards = `https://wakatime.com/api/v1/users/current/leaderboards`
 const lineBot = (token, meesage) => {
@@ -11,10 +13,13 @@ module.exports = {
   path: '/api/leaderbord/:key/:id',
   handler: async (req) => {
     const { id, key } = req.params
+    logger.start('api leaderbord')
     const { data: { data: leaderbords } } = await axios(`${wakaApiLeaderboards}/${id}?api_key=${key}`)
     const flex = wakaRank(leaderbords)
-    lineBot(process.env.NODE_ENV == 'development' ? 'kem' : 'sru', flex).catch(ex => {
-      console.log(ex.response)
+    lineBot(process.env.NODE_ENV == 'development' ? 'kem' : 'sru', flex).then(() => {
+      logger.success('notify send.')
+    }).catch(ex => {
+      logger.error(ex.response)
     })
     return flex
   }
